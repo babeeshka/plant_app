b2bf8da8e75d39ed833178aeb6651039a9f681e9
Walkah <walkerbabich@gmail.com>
2023-05-07 11:21:37 -0400

    fetch plant changes

--- plant_info.py
+++ plant_info.py
@@ -1,63 +1,72 @@
+from config import TREFLE_API_BASE_URL
+
+import os
 import requests
 
-from config import TREFLE_API_BASE_URL
-import os
+DESIRED_FIELDS = [
+    'id', 'common_name', 'scientific_name', 'family_common_name', 'bibliography', 'author', 'year', 'family',
+    'genus', 'duration', 'growth_habit', 'growth_rate', 'edible', 'vegetable', 'observations',
+    'flower_color', 'images'
+]
 
 
-def get_plant_info(plant_name):
-    """Fetches plant information from the Trefle API"""
-    url = f"{TREFLE_API_BASE_URL}/plants/search?q={plant_name}&token={os.environ.get('TREFLE_TOKEN')}"
-    response = requests.get(url)
-    print(response)
+def get_plant_info(query):
+    query = query.strip().lower()
+
+    # Try searching by common name first
+    endpoint = f"{TREFLE_API_BASE_URL}/plants&token={os.environ.get('TREFLE_TOKEN')}&filter[common_name]={query}"
+    response = requests.get(endpoint)
 
     if response.status_code != 200:
-        print(f"no plant found, status code: {response.status_code}, response: {response.content}")
         return None
 
-    # List of desired fields
-    desired_fields = ["common_name", "scientific_name", "growth.light", "growth.water", "growth.temperature",
-                      "growth.humidity", "growth.growing_days", "propagation", "pests", "image_url"]
+    results = response.json().get('data', [])
 
-    # Find record with highest score
-    max_score = 0
-    max_score_data = None
-    for data in response.json()["data"]:
-        score = 0
-        for field in desired_fields:
-            if data.get(field):
-                score += 1
-        if score > max_score:
-            max_score = score
-            max_score_data = data
+    # If no results are found with common name search, try searching by scientific name
+    if not results:
+        endpoint = f"{TREFLE_API_BASE_URL}/plants&token={os.environ.get('TREFLE_TOKEN')}&filter[scientific_name]={query}"
+        response = requests.get(endpoint)
 
-    if not max_score_data:
-        print(f"no matching plant found")
+        if response.status_code != 200:
+            return None
+
+        results = response.json().get('data', [])
+
+    if not results:
         return None
 
-    # Extract fields from highest score data
-    plant = {
-        "common_name": max_score_data.get("common_name"),
-        "scientific_name": max_score_data.get("scientific_name"),
-        "sunlight_care": max_score_data.get("growth.light"),
-        "water_care": max_score_data.get("growth.water"),
-        "temperature_care": max_score_data.get("growth.temperature"),
-        "humidity_care": max_score_data.get("growth.humidity"),
-        "growing_tips": max_score_data.get("growth.growing_days"),
-        "propagation_tips": max_score_data.get("propagation", {}).get("vegetative", {}).get("cutting", "Unknown"),
-        "common_pests": max_score_data.get("observations"),
-        "image_url": max_score_data.get("image_url"),
-        "family": max_score_data["family"].get("name") if isinstance(max_score_data.get("family"), dict) else None,
-        "genus": max_score_data["genus"]["name"] if isinstance(max_score_data.get("genus"), dict) else None,
-        "year": max_score_data.get("year"),
-        "edible": bool(max_score_data.get("edible")),
-        "edible_part": max_score_data.get("edible_part"),
-        "edible_notes": max_score_data.get("edible_notes"),
-        "medicinal": bool(max_score_data.get("medicinal")),
-        "medicinal_notes": max_score_data.get("medicinal_notes"),
-        "toxicity": max_score_data.get("toxicity"),
-        "synonyms": max_score_data.get("synonyms"),
-        "native_status": max_score_data.get("distribution", {}).get("native", "Unknown"),
-        "conservation_status": max_score_data["conservation_status"] if isinstance(
-            max_score_data.get("conservation_status"), dict) else None,
-    }
-    return plant
+    # Sort the results by the number of desired fields present in the result
+    sorted_results = sorted(results, key=lambda x: -sum([1 for field in DESIRED_FIELDS if (field in x and x[field]) or (
+            field == 'images' and x.get('images', {}).get('url'))]))
+
+    best_matching_plant = sorted_results[0]
+    plant_id = best_matching_plant['id']
+
+    # Fetch the detailed information for the best matching plant
+    detailed_endpoint = f"{TREFLE_API_BASE_URL}/plants/{plant_id}?token={os.environ.get('TREFLE_TOKEN')}"
+    detailed_response = requests.get(detailed_endpoint)
+
+    if detailed_response.status_code != 200:
+        return None
+
+    detailed_plant_info = detailed_response.json().get('data', {})
+
+    # Extract only the desired fields from the detailed plant information
+    extracted_info = {field: detailed_plant_info.get(field) for field in DESIRED_FIELDS}
+
+    # Extract the image URL from the images object
+    images = extracted_info.get('images')
+    if images and 'url' in images:
+        extracted_info['image_url'] = images['url']
+    else:
+        extracted_info['image_url'] = None
+    del extracted_info['images']
+
+    # Convert the durations list into a comma-separated string
+    durations = extracted_info.get('durations')
+    if durations:
+        extracted_info['durations'] = ', '.join(durations)
+    else:
+        extracted_info['durations'] = None
+
+    return extracted_info
